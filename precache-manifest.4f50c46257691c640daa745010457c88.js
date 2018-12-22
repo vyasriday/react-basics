96de2007d09563f1f99351e36a04b5469666dbc0
Hridayesh Sharma <vyasriday7@gmail.com>
2018-12-22 23:13:58 +0530

    Updates

---
+++ precache-manifest.4f50c46257691c640daa745010457c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8b48bc4a9b574479780",
+    "url": "/static/js/main.e8b48bc4.chunk.js"
+  },
+  {
+    "revision": "48287269115a63235df5",
+    "url": "/static/js/1.48287269.chunk.js"
+  },
+  {
+    "revision": "e8b48bc4a9b574479780",
+    "url": "/static/css/main.a8b8a454.chunk.css"
+  },
+  {
+    "revision": "188d496cbd983db76c4250718010cecb",
+    "url": "/index.html"
+  }
+];
